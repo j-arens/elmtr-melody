1851bea4aca10c511e5509a92c08bd0d3b327ae0
Josh <arens.joshua@sbcglobal.net>
2018-03-17 20:04:28 -0400

    remove track favorite

--- assets/src/adapter/tracks/defaults.ts
+++ assets/src/adapter/tracks/defaults.ts
@@ -2,7 +2,6 @@ const { MELODY_ENV: { pluginsUrl } } = (window as any);
 
 export const defaultTrack = {
     source_url: `${pluginsUrl}/elmtr-melody/assets/audio/placeholder-track.mp3`,
-    favorite: false,
     artwork: '',
     media_details: {
         artist: 'Artist',
