2a8fafa62fd058fb0a6bf5c87ff04979e0dc3270
Josh <arens.joshua@sbcglobal.net>
2018-04-26 22:08:50 -0400

    lazy load slides

---
+++ src/melody/components/Artwork/Slideshow/LazyLoadSlide.tsx
@@ -0,0 +1,46 @@
+import { WithOptionalClassName } from '@components/type';
+import { Track } from '@redux/type';
+import { h } from 'preact';
+
+interface Props extends WithOptionalClassName {
+    currentTrack: number;
+    totalTracks: number;
+    index: number;
+    className?: string;
+    children?: JSX.Element[];
+}
+
+const EmptySlide = (className: string): JSX.Element =>
+    <li class={className} />;
+
+const LoadedSlide = (
+    className: string,
+    children: JSX.Element[],
+): JSX.Element => (
+    <li class={className}>
+        {children}
+    </li>
+);
+
+export default ({
+    currentTrack,
+    totalTracks,
+    index,
+    className = '',
+    children,
+}: Props) => {
+    const loadLast = currentTrack === 0 && index === (totalTracks - 1);
+    const loadFirst = currentTrack === (totalTracks - 1) && index === 0;
+    const aboveRange = index > (currentTrack + 1);
+    const belowRange = index < (currentTrack - 1);
+
+    if (loadLast || loadFirst) {
+        return LoadedSlide(className, children);
+    }
+
+    if (aboveRange || belowRange) {
+        return EmptySlide(className);
+    }
+
+    return LoadedSlide(className, children);
+};
--- src/melody/components/Artwork/Slideshow/Slideshow.tsx
+++ src/melody/components/Artwork/Slideshow/Slideshow.tsx
@@ -2,6 +2,7 @@ import ResponsiveBgImage from '@components/Artwork/ResponsiveBgImage';
 import { WithOptionalClassName } from '@components/type';
 import { Track } from '@redux/type';
 import { h } from 'preact';
+import LazyLoadSlide from './LazyLoadSlide';
 const throttle = require('lodash.throttle');
 const s = require('./style.scss');
 
@@ -14,23 +15,26 @@ export default ({
     tracks,
     currentTrack,
     className = '',
-}: Props) => {
-    return (
-        <ul
-            class={`${s.Slideshow} ${className}`}
-            data-melody-slideshow
-            style={{
-                transform: `translate3d(${currentTrack * -100}%, 0px, 0px)`,
-            }}
-        >
-            {tracks.map((track, i) => (
-                <li class={s.Slideshow__slide}>
-                    <ResponsiveBgImage
-                        className={s.Slideshow__image}
-                        artwork={track.artwork}
-                    />
-                </li>
-            ))}
-        </ul>
-    );
-};
+}: Props) => (
+    <ul
+        class={`${s.Slideshow} ${className}`}
+        data-melody-slideshow
+        style={{
+            transform: `translate3d(${currentTrack * -100}%, 0px, 0px)`,
+        }}
+    >
+        {tracks.map((track, i) => (
+            <LazyLoadSlide
+                index={i}
+                totalTracks={tracks.length}
+                className={s.Slideshow__slide}
+                currentTrack={currentTrack}
+            >
+                <ResponsiveBgImage
+                    className={s.Slideshow__image}
+                    artwork={track.artwork}
+                />
+            </LazyLoadSlide>
+        ))}
+    </ul>
+);
