1923af96396921ee9509b9ebef9d5a258f514767
Josh <arens.joshua@sbcglobal.net>
2018-05-11 23:05:41 -0400

    wip

--- dev/elementor.js
+++ dev/elementor.js
@@ -1,15 +1,15 @@
-const octokit = require('@octokit/rest')();
 const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const url = require('url');
 
 const OWNER = 'pojome';
 const REPO = 'elementor';
-const PATH = path.resolve(process.cwd(), './dev/elementor');
-const DOWNLOAD_PATH = path.resolve(process.cwd(), './dev/temp');
+const DOWNLOAD_DIR = path.resolve(process.cwd(), './dev/temp');
+const DOWNLOAD_PATH = path.resolve(DOWNLOAD_DIR, 'elementor.zip');
+const INSTALL_DIR = path.resolve(process.cwd(), './dev/elementor');
 
 process.on('unhandledRejection', () => {});
 
@@ -21,72 +21,98 @@ const styles = {
 const paths = {
     exists: path => fs.existsSync(path),
     make: path => exec(`mkdir ${path}`),
-    clear: () => exec(`rm -rf ${PATH}/*`),
+    // clear: path => execSync(`rm -rf ${path}/*`),
 }
 
-/**
- * 
- */
-const installZip = () => console.log('install zip');
+const makeReqParams = href => {
+    const { host, path } = url.parse(href);
+    return {
+        host,
+        path,
+        headers: {
+            'user-agent': 'elementor', // github api requirement
+        },
+    };
+};
 
-/**
- * 
- * @param {*} req 
- */
-const streamToTemp = req => {
-    console.log(req);
-    if (!paths.exists(DOWNLOAD_PATH)) {
-        paths.make(DOWNLOAD_PATH);
+const handleRedirect = next => res => {
+    const { headers: { status } } = res;
+    const statusCode = parseInt(status);
+    if ((statusCode >= 300) && (statusCode <= 307)) {
+        const { headers: { location } } = res;
+        console.log(styles.info(`↩️ Following redirect to ${location}`));
+        const params = makeReqParams(location);
+        https.get(params, handleRedirect(next));
+        return;
     }
-    // fs.createWriteStream(path.resolve(`${DOWNLOAD_PATH}`, 'elementor.zip')).on('close', installZip);
-    const dest = fs.createWriteStream(path.resolve(`${DOWNLOAD_PATH}`, 'elementor.zip'), { encoding: 'binary' });
-    req.on('data', data => {
-        console.log('data');
-        data => dest.write(data);
-    });
-    req.on('end', () => {
-        console.log('end');
-        dest.end();
-        installZip();
-    });
-}
+    next(res);
+};
 
-/**
- * 
- * @param {*} param0 
- */
+const pick = (json, index) => {
+    if (!json.length) {
+        console.log(styles.error('😱 No releases available!'));
+        process.exit(0);
+    }
+    if ((index < 0) || (index > (json.length - 1))) {
+        console.log(styles.error(`😱 No release at index ${index}!`));
+        process.exit(0);
+    }
+    return json[index];
+};
+
+const installZip = () => {
+    // if (paths.exists(INSTALL_PATH)) {
+    //     paths.clear(INSTALL_PATH);
+    // }
+
+    console.log(styles.info(`🌟 Extracting to ${INSTALL_DIR}`));
+    execSync(`unzip ${DOWNLOAD_PATH} -d ${INSTALL_DIR}`);
+    execSync(`cd ${INSTALL_DIR} && mv "\$(ls | head -1)"/* .`, (e, stdout, stderr) => {
+        console.log('e', e);
+        console.log('stdout', stdout);
+        console.log('stderr', stderr);
+    });
+    // execSync(`rm -rf ${INSTALL_DIR}`)
+};
+
+const streamToTemp = res => {
+    if (!paths.exists(DOWNLOAD_DIR)) {
+        paths.make(DOWNLOAD_DIR);
+    }
+    const dest = fs.createWriteStream(DOWNLOAD_PATH, { encoding: 'binary' });
+    res.pipe(dest);
+    res.on('end', installZip);
+};
+
 const download = ({ name, zipball_url }) => {
-    console.log(styles.info(`Downloading tagged release ${name}...`));
+    console.log(styles.info(`⬇️ Downloading release tagged ${name}...`));
     try {
-        const { host, path } = url.parse(zipball_url);
-        const params = {
-            host,
-            path, 
-            headers: {
-                'user-agent': 'elementor',
-            },
-        };
-        https.get(params, streamToTemp);
+        const params = makeReqParams(zipball_url);
+        https.get(params, handleRedirect(streamToTemp));
     } catch (e) {
-        console.log(styles.error('Download of Elementor failed!'));
+        console.log(styles.error('😱 Download of Elementor failed!'));
         console.error(Error(e));
     }
-}
+};
 
 /**
  * 
  */
 module.exports.getLatest = async () => {
-    console.log(styles.info('Getting the latest release of elementor...'));
+    console.log(styles.info('📡 Getting the latest release of elementor...'));
     try {
-        const res = await octokit.repos.getTags({ owner: OWNER, repo: REPO });
-        if (!res.data.length) {
-            console.log(styles.error('No releases available!'));
-            process.exit(0);
-        }
-        download(res.data[0]);
+        const params = makeReqParams(`https://api.github.com/repos/${OWNER}/${REPO}/tags`);
+        https.get(params, res => {
+            let body = '';
+            res.setEncoding('utf8');
+            res.on('data', data => body += data);
+            res.on('end', () => {
+                const release = pick(JSON.parse(body), 0);
+                download(release);
+            });
+        });
     } catch (e) {
-        console.log(styles.error('Unable to get latest Elementor release!'));
+        console.log(styles.error('😱 Unable to get latest Elementor release!'));
         console.error(Error(e));
     }
-}
+};
