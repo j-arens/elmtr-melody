877e6d0f36b99532a8c7e83a70d978e919f3458a
Josh <arens.joshua@sbcglobal.net>
2018-08-13 19:31:49 -0400

    prev btn skips to beginning when less than two tracks loaded

--- src/melody/components/controls/PrevButton/index.ts
+++ src/melody/components/controls/PrevButton/index.ts
@@ -2,18 +2,14 @@ import { WithOptionalClassName } from '@components/type';
 import { prevTrack } from '@redux/modules/audio/actions';
 import { State } from '@redux/type';
 import { connect } from 'preact-redux';
-import PrevButton, { DispatchProps, StateProps } from './PrevButton';
-
-const mapStateToProps = (state: State) => ({
-    totalTracks: state.audio.tracks.length,
-});
+import PrevButton, { DispatchProps } from './PrevButton';
 
 const mapDispatchToProps = dispatch => ({
     prevTrack: () => dispatch(prevTrack()),
 });
 
 export default connect<
-    StateProps,
+    {},
     DispatchProps,
     WithOptionalClassName
->(mapStateToProps, mapDispatchToProps)(PrevButton);
+>(null, mapDispatchToProps)(PrevButton);
